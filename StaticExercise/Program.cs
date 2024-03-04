99993e9acc12864149b8dc7eefbe673b7b337bc1
navpreet <navpreetbatth2.nb@gmail.com>
2024-03-04 14:09:52 -0800

    initial commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TempConverter.FahrenheitToCelsius(100.00);
+            // if no consoleWriteline in method, we can also implement here
+            //ConsoleWriteLine((TempConverter.FahrenheitToCelsius(100.00));
+
+            TempConverter.CelsiusToFahrenheit(37.78);
+
+            //if user input is needed to give output
+            Console.WriteLine("Enter in celsius degree : ");
+            double userInput = double.Parse(Console.ReadLine());
+            TempConverter.CelsiusToFahrenheit(userInput);
+
+
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fTemp)
+        {
+            double celcius = (fTemp - 32) * 5 / 9;
+            Console.WriteLine(celcius);
+            return celcius;
+        }
+        public static double CelsiusToFahrenheit(double cTemp)
+        {
+            double fahrenheit = (cTemp * 9) / 5 + 32;
+            Console.WriteLine(fahrenheit);
+            return fahrenheit;
+        }
+        
+    }
+}
